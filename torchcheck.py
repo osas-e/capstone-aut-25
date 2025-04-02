a765940fa7cea62193b64a4a4348d6428a6f0706
osas-e <oeguaibor@gmail.com>
2025-04-02 14:19:01 +1100

    Clean up code

--- torchcheck.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# Verify MPS support
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    print("M1 GPU is available and PyTorch is configured to use MPS!")
-else:
-    print("MPS backend is not available. Check your setup.")
